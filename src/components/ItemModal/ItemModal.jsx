a33663bd4c1db5bad4cf69f045083c5cd1d24e5c
E2Funny14 <eianjennings@gmail.com>
2025-04-05 20:04:35 -0400

    modal functions and weather added

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -29,7 +29,7 @@ function ItemModal({ activeModal, onClose, card }) {
         </button>
         <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
+          <p className="modal__caption">{card.name}</p>
           <p className="modal__weather">Weather: {card.weather}</p>
         </div>
       </div>
